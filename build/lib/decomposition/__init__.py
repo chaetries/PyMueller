f95298b736b24e572cf19cdb62cd266a2c0ab919
chaetries <a40075@outlook.com>
2025-02-12 13:41:46 +0100

    Initial commit

---
+++ build/lib/decomposition/__init__.py
@@ -0,0 +1,1 @@
+from .decomposition import lu_chipman
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymueller",
-    version="0.1.0",
+    version="0.1.1",
     description="Tools and functions for 4x4 Mueller matrices",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
