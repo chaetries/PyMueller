1d2e4d660b7bc178ca933b7fa1369187caaa1622
chaetries <a40075@outlook.com>
2025-02-18 09:14:03 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymueller",  # The name of your package
-    version="0.1.1",   # Your current version
+    version="0.1.2",   # Your current version
     description="Tools and functions for 4x4 Mueller matrices",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
